9950f3b52fc6a93ed76030220b5d439df20f415f
CodyCork <codycork.sfg@outlook.com>
2022-10-01 15:26:54 -0500

    home page update

--- src/Home.js
+++ src/Home.js
@@ -6,10 +6,21 @@ function Home() {
   return (
     <div className="Home">
       <header className="App-header">
-        <h1 id="title">HACKER CONNECTED</h1>
+        <ul id="navbar">
+          <a href="">Home</a>
+          <a href="">About</a>
+          <a href="">Sign In</a>
+        </ul>
         <div>
-          <nav><a>Home</a></nav>
-          <nav><a>About</a></nav>
+          <img id="logo"></img>
+          <h1 id="title">HACKER CONNECTED</h1>
+        </div>
+        <div>
+          <p>What are you looking for?</p>
+          <form id="searchbar">
+            <input></input>
+            <button>Search</button>
+          </form>
         </div>
       </header>
     </div>
