74c9305395f88945d262d04fd38daf6426753241
CodyCork <codycork.sfg@outlook.com>
2022-10-02 21:26:01 -0500

    Navbar update (bootstrap)

--- src/Home.js
+++ src/Home.js
@@ -5,24 +5,90 @@ import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 function Home() {
   return (
     <div className="Home">
-      <header className="App-header">
-        <ul id="navbar">
-          <a href="">Home</a>
-          <a href="">About</a>
-          <a href="">Sign In</a>
-        </ul>
+      <header className="home-header">
+        <div id="navbar">
+          <nav class="navbar navbar-expand-lg bg-light">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#">
+                <img src="./public/hackerclogo.jpg" alt="hacker logo"></img>
+              </a>
+              <button
+                class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">
+                      Home
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">
+                      Link
+                    </a>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a
+                      class="nav-link dropdown-toggle"
+                      href="#"
+                      role="button"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      Dropdown
+                    </a>
+                    <ul class="dropdown-menu">
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          Action
+                        </a>
+                      </li>
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          Another action
+                        </a>
+                      </li>
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          Something else here
+                        </a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link disabled">Disabled</a>
+                  </li>
+                </ul>
+                <form class="d-flex" role="search">
+                  <input
+                    class="form-control me-2"
+                    type="search"
+                    placeholder="Search"
+                    aria-label="Search"
+                  ></input>
+                  <button class="btn btn-outline-success" type="submit">
+                    Search
+                  </button>
+                </form>
+              </div>
+            </div>
+          </nav>
+        </div>
+
         <div>
-          <img id="logo"></img>
           <h1 id="title">HACKER CONNECTED</h1>
         </div>
-        <div id="searchbar">
-          <p>
-            What are you looking for?
-            <form id="">
-              <input></input>
-              <button>Search</button>
-            </form>
-          </p>
+
+        <div id="welcome">
+          <h2>Welcome to the social network for hackers!</h2>
         </div>
       </header>
     </div>
