748b5b8f6f92bfd62b5a1dff7b3edbc865784352
CodyCork <codycork.sfg@outlook.com>
2022-10-03 20:39:45 -0500

    added .env and page templates

--- src/index.js
+++ src/index.js
@@ -1,20 +1,16 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Home from "./Home";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
+import About from "./About";
+import Search from "./Search";
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import './index.css';
-import Home from './Home';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import About from './About'
-import Search from './Search'
-
-const userRoute= require("./route/profile")
-const autRoute= require("./route/aut")
+const userRoute = require("./route/profile");
+const autRoute = require("./route/aut");
 
 const router = createBrowserRouter([
   {
@@ -42,9 +38,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
+app.use("/api/profile", userRoute);
+app.use("/aut", autRoute);
 
-
-app.use("/api/profile", userRoute)
-app.use("/aut", autRoute)
-
-module.exports=routerpw
+module.exports = routerpw;
