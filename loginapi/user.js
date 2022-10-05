60a7c3c6ab29d8e71c1fc59de38881faaf9d5e26
balthier <rrodrijose@gmail.com>
2022-10-05 11:12:27 -0500

    user route and controller

---
+++ loginapi/user.js
@@ -0,0 +1,9 @@
+const express= require('express');
+const { Model } = require('sequelize');
+const router = express.Router();
+const userController = require('../controller/usercon')
+
+
+
+
+Model.exports = router;
