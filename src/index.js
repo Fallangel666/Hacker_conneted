076d213bc6e321adc43eaea009367089510dee90
Fallanagel <havemedance@hotmail.com>
2022-10-08 11:26:37 -0500

    navbar fixed"
    "

--- src/index.js
+++ src/index.js
@@ -1,26 +1,33 @@
 
 import './index.css';
-import Home from './Home';
 import reportWebVitals from './reportWebVitals';
+import Home from './Home';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from ".Layout";
+import Home from "./Home";
+import About from "./About";
+import Search from "./Search";
+import NoPage from ".NoPage";
 
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="search" element={<Search />} />
+          <Route path="about" element={<About />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
-ReactDOM.render(
-  <BrowserRouter>
-    {<Home/>}
-  </BrowserRouter>,
-   document.getElementById('root')
-  )
-;
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-// App.use("/profile", userRoute)
-// App.use("/aut", autRoute)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />)
 
-//module.exports=router
