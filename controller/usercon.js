1242f4a3f113b5aadea479f8f464f1e89c5427d4
balthier <rrodrijose@gmail.com>
2022-10-07 21:27:30 -0500

    control continuation

--- controller/usercon.js
+++ controller/usercon.js
@@ -1,4 +1,26 @@
 import user from "../loginmodel/user"
 
 //get user
-export const getUser = async(req, res)
+export const getUser = async(req, res)=> {
+    const id =req.params.id;
+
+    try{
+    const user =await user.findByID(id);
+
+    if(user)
+        {
+            res.status().json(user)
+    }
+    else{
+        res.status(404).json("none existant user")
+    }
+}   catch(error){
+    res.status().json(user)
+}
+}
+
+//user update
+export const updateUser= async(req,res)=>{
+    const id= req.params.id
+    const {currentUserID, password}= req.body
+}
--- routes/userroute.js
+++ routes/userroute.js
@@ -1,5 +1,8 @@
 import express from "express";
+import {getuser} from "../controller/usercon"
 
 const router= express.Router();
+
+router.get('/:id', getuser)
 
 export default router
