902fcad19277a3bcea7952a76fc2a46b506e025c
CodyCork <codycork.sfg@outlook.com>
2022-10-09 17:04:38 -0500

    deleted extra html

--- client/src/About.js
+++ client/src/About.js
@@ -1,17 +1,22 @@
+import { Navbar } from "react-bootstrap";
+import NavBar from "./Navbar.jsx"
 function About() {
-    return (
-      <div className="Home">
-        <header className="home-header">
-            
-          <div>
-            <h1 id="title">HACKER CONNECTED</h1>
-          </div>
-          <div id="welcome">
-            <h2>Welcome to the social network for hackers!</h2>
-          </div>
-        </header>
-      </div>
-    );
-  }
-  
-  export default About;
+  return (
+    <div className="Home">
+      <header className="home-header">
+        <div id="navbar">
+          <NavBar />
+        </div>
+
+        <div>
+          <h1 id="title">HACKER CONNECTED</h1>
+        </div>
+        <div id="aboutTitle">
+          <h1>About</h1>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default About;
