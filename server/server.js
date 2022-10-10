0dd50cf0e837ae013178f17f809fdd69d8af831e
CodyCork <codycork.sfg@outlook.com>
2022-10-09 23:07:30 -0500

    merge

--- server/server.js
+++ server/server.js
@@ -1,27 +1,29 @@
 import express from "express";
-import bodyParser from 'body-parser';
+import bodyParser from "body-parser";
 //sql import
 import dotenv from "dotenv";
-import authroute from './routes/authroute.js'
-import userroute from  '../routes/userroute.js'
+import authroute from "./routes/authroute.js";
+import userroute from "../routes/userroute.js";
 
 //routes
 
 const app = express();
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 //midware
-app.use (bodyParser.json({limit: '30mb', extended:true}))
-app.use(bodyParser.urlencoded({limit: '30mb', extended:true}))
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
-dotenv.config()
+dotenv.config();
 
 //sqldb goes here
 
 //import { alignPropType } from "react-bootstrap/esm/types";
 
 //toute usage
-app.use('/auth', authroute)
-app.use('/user', userroute)
-app.use('/post', postroute)
+app.use("/auth", authroute);
+app.use("/user", userroute);
+app.use("/post", postroute);
 
-app.listen(PORT)
+app.listen(PORT, () => {
+  console.log("Running on PORT 3001");
+});
